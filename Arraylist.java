d56551ec1215578db890f8b5b717dd9f5b72111c
ASHAMAINA SWETHA <ashamainaswetha17@gmail.com>
2025-08-18 21:19:15 +0530

    Create Arraylist.java

---
+++ Arraylist.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Arraylist{
+    public static void main(String args[]){
+        ArrayList<Integer> arr=new ArrayList<>();
+        arr.add(5);
+        arr.add(9);
+        arr.add(0);
+        arr.add(8);
+        System.out.println(arr);
+    }
+}
