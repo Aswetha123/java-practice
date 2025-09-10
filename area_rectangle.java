9fda9140b9e1f4956080f07fe3b3434d1845f4e4
ASHAMAINA SWETHA <ashamainaswetha17@gmail.com>
2025-09-10 20:47:12 +0530

    Create area_rectangle.java

---
+++ area_rectangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class rectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the length value:");
+		int length = sc.nextInt();
+		System.out.println("Enter the breadth value:");
+		int breadth = sc.nextInt();
+		int area= length*breadth;
+		System.out.println("value of area is:"+area);
+		
+		
+
+	}
+
+}
