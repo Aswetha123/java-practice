06bc754cbd721b950637ebe9edc48d6e9d03b122
Vijay Swatha <vijay.swatha@example.com>
2025-09-16 11:53:05 +0530

    bank

---
+++ while.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class whileprogram {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n=1;
+		while(n <= 99)
+		{
+			System.out.println(n);
+			n++;
+		}
+	}
+
+}
